18976a7550cc5ba9f1b22de5e3cd1eafea7cc326
pattex16 <iacobuccivalerio@gmail.com>
2021-07-16 18:45:33 +0200

    modified:   bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,6 @@
+import os
+token = open(os.environ["HOME"] + "/.local/secret/telegram_token","r").read().strip()
+
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
@@ -6,7 +9,7 @@ def hello(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Ciao " + update.effective_user.name + " e benvenuto!!!")
 
 
-updater = Updater('1723752355:AAHolWxexkvvnujPJjzk0hgwG8c4a6l_yHo')
+updater = Updater('')
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
